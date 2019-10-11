44c81da700529a827c2f8957eca9d1d3636c9549
Ubuntu <ubuntu@ip-172-31-86-196.ec2.internal>
2019-10-11 12:37:31 +0000

    added row chart

--- script.js
+++ script.js
@@ -24,6 +24,7 @@ queue()
         show_year_selector(ndx);
         show_total_no_of_suicides(ndx)
         show_no_of_suicides_by_gender(ndx)
+        show_countries_with_highest_suicide(ndx)
 
         dc.renderAll();
     });
@@ -84,7 +85,7 @@ function show_total_no_of_suicides(ndx) {
 
 //Pie chart to show the percentage and the no of suicides for each gender
 function show_no_of_suicides_by_gender(ndx) {
-    // Data dimension for gender
+    //Data dimension for gender
     let gender_dim = ndx.dimension(dc.pluck('sex'));
     //Data group for no of suicides per 100k people for each gender
     let total_suicides_each_gender = gender_dim.group().reduceSum(dc.pluck('suicides_100k'));
@@ -101,5 +102,30 @@ function show_no_of_suicides_by_gender(ndx) {
                 return d.data.key.charAt(0).toUpperCase() + d.data.key.substring(1) + ': ' + d.data.value + ' (' + dc.utils.printSingleValue((d.endAngle - d.startAngle) / (2 * Math.PI) * 100) + '%)';
             });
         });
+}
 
+
+//Row chart to show the top 10 countries with the highest suicide rate
+function show_countries_with_highest_suicide(ndx) {
+    //Data dimension for country
+    let country_dim = ndx.dimension(dc.pluck('country'));
+    //Data group for no of suicides per 100k people for each country
+    let total_suicide_by_country = country_dim.group().reduceSum(dc.pluck('suicides_100k'));
+    //To set the number of countries in the chart
+    let top_countries = 10;
+
+    dc.rowChart("#row-chart")
+        .width(300)
+        .height(450)
+        .margins({ top: 20, left: 10, right: 10, bottom: 20 })
+        .transitionDuration(750)
+        .dimension(country_dim)
+        .group(total_suicide_by_country)
+        .data(function(d) { return d.top(top_countries); })
+        .renderLabel(true)
+        .gap(1)
+        .title(function(d) { return "No. of suicides: " + d.value; })
+        .elasticX(true)
+        .useViewBoxResizing(true)
+        .xAxis().ticks(10).tickFormat(d3.format("s"));
 }
